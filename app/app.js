0850fc0f07923584f802a62c3e6d061b424be482
Insane <luojieyy@gmail.com>
2014-12-29 19:40:21 +0800

    using ui-router

--- app/app.js
+++ app/app.js
@@ -4,10 +4,35 @@
 * Description
 */
 angular.module('Fireforum', ['ngRoute', 'ui.router'])
+.run(['$rootScope', function($rootScope){
+
+}])
+.config(function($stateProvider, $urlRouterProvider) {
+	$urlRouterProvider.otherwise("/");
+	// Now set up the states
+	$stateProvider
+	.state('home', {
+		url: "/",
+		templateUrl: "templates/index.html"
+	})
+	.state('users', {
+		url: "/users",
+		templateUrl: "templates/users/index.html"
+	})
+	.state('todos', {
+		url: "/todos",
+		templateUrl: "templates/todos/index.html"
+	})
+	.state('forum', {
+		url: "/forum",
+		templateUrl: "templates/forum/index.html"
+	});
+
+});
 
 /**
  *   bootstrap on ready                              
  */
 $(document).ready(function(){
-	angular.bootstrap(document, ['fireforum'])
+	angular.bootstrap(document, ['Fireforum'])
 });
